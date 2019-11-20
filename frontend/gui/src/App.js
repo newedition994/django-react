68eaad583840acd147a2faad44fe80af362d3157
Breyanna Vass <breyannavass@gmail.com>
2019-11-20 18:21:58 -0500

    modify the containers

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 import "antd/dist/antd.css";
 
 import CustomLayout from "./containers/Layout";
+import ArticleList from "./containers/ArticleListView";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CustomLayout />
+        <CustomLayout>
+          <ArticleList />
+        </CustomLayout>
       </div>
     );
   }
---
+++ frontend/gui/src/containers/ArticleListView.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Article from "../components/Article";
+
+class ArticleList extends React.Component {
+  render() {
+    return <Article />;
+  }
+}
+
+export default ArticleList;
