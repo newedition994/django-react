8430ca7ea08bb3880011c9cba38f766d17972c11
Breyanna Vass <breyannavass@gmail.com>
2019-11-20 17:35:34 -0500

    making components and containers with a layout

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import "antd/dist/antd.css";
+
+import CustomLayout from "./containers/Layout";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Connect</h1>
+        <CustomLayout />
       </div>
     );
   }
