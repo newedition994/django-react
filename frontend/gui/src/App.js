663c5c0d16f9e5d95f1824962cd5037abe44e43a
Breyanna Vass <breyannavass@gmail.com>
2019-11-20 18:50:06 -0500

    taking in the routes on the app

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
-
-import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { connect } from "react-redux";
+import BaseRouter from "./routes";
 import "antd/dist/antd.css";
+import * as actions from "./store/actions/auth";
 
 import CustomLayout from "./containers/Layout";
-import ArticleList from "./containers/ArticleListView";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
     return (
-      <div className="App">
-        <CustomLayout>
-          <ArticleList />
-        </CustomLayout>
+      <div>
+        <Router>
+          <CustomLayout {...this.props}>
+            <BaseRouter />
+          </CustomLayout>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
