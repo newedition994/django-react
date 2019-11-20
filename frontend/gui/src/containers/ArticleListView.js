5ac219767aa311e8d79f96b0111c96b8c841fb45
Breyanna Vass <breyannavass@gmail.com>
2019-11-20 18:29:13 -0500

    fixing the article list view

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -1,9 +1,39 @@
 import React from "react";
-import Article from "../components/Article";
+import axios from "axios";
+import Articles from "../components/Article";
+import CustomForm from "../components/Form";
 
 class ArticleList extends React.Component {
+  state = {
+    articles: []
+  };
+
+  fetchArticles = () => {
+    axios.get("http://127.0.0.1:8000/api/").then(res => {
+      this.setState({
+        articles: res.data
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.fetchArticles();
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.token) {
+      this.fetchArticles();
+    }
+  }
+
   render() {
-    return <Article />;
+    return (
+      <div>
+        <Articles data={this.state.articles} /> <br />
+        <h2> Create an article </h2>
+        <CustomForm requestType="post" articleID={null} btnText="Create" />
+      </div>
+    );
   }
 }
 
