e39947dd80e2ac6f70d7089ff39af431e0ee775e
Breyanna Vass <breyannavass@gmail.com>
2019-11-20 18:27:32 -0500

    made a form file

--- frontend/gui/src/components/Article.js
+++ frontend/gui/src/components/Article.js
@@ -1,27 +1,19 @@
 import React from "react";
 import { List, Avatar, Icon } from "antd";
 
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: "http://ant.design",
-    title: `ant design part ${i}`,
-    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    description:
-      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
-    content:
-      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently."
-  });
-}
-
 const IconText = ({ type, text }) => (
   <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
+    <Icon
+      type={type}
+      style={{
+        marginRight: 8
+      }}
+    />
     {text}
   </span>
 );
 
-const Article = props => {
+const Articles = props => {
   return (
     <List
       itemLayout="vertical"
@@ -32,19 +24,14 @@ const Article = props => {
         },
         pageSize: 3
       }}
-      dataSource={listData}
-      footer={
-        <div>
-          <b>ant design</b> footer part
-        </div>
-      }
+      dataSource={props.data}
       renderItem={item => (
         <List.Item
           key={item.title}
           actions={[
-            <IconText type="star-o" text="156" key="list-vertical-star-o" />,
-            <IconText type="like-o" text="156" key="list-vertical-like-o" />,
-            <IconText type="message" text="2" key="list-vertical-message" />
+            <IconText type="star-o" text="156" />,
+            <IconText type="like-o" text="156" />,
+            <IconText type="message" text="2" />
           ]}
           extra={
             <img
@@ -56,7 +43,7 @@ const Article = props => {
         >
           <List.Item.Meta
             avatar={<Avatar src={item.avatar} />}
-            title={<a href={item.href}>{item.title}</a>}
+            title={<a href={`/articles/${item.id}`}> {item.title} </a>}
             description={item.description}
           />
           {item.content}
@@ -66,4 +53,4 @@ const Article = props => {
   );
 };
 
-export default Article;
+export default Articles;
